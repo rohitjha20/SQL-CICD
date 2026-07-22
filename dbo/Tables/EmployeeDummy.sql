762941485a69ec1fb475d8560c271a818d641e5c
Rohit Jha <53367041+rohitjha20@users.noreply.github.com>
2026-07-22 16:39:44 +0530

    Add PhoneNumber column to EmployeeDummy table

--- dbo/Tables/EmployeeDummy.sql
+++ dbo/Tables/EmployeeDummy.sql
@@ -5,6 +5,7 @@ CREATE TABLE [dbo].[EmployeeDummy] (
     [Salary]       DECIMAL (10, 2) NULL,
     [JoiningDate]  DATE            NULL,
     [EmailID]       NVARCHAR (200) NULL,
+    [PhoneNumber]   NVARCHAR (15)  NULL,
     PRIMARY KEY CLUSTERED ([EmployeeID] ASC)
 );
 GO
