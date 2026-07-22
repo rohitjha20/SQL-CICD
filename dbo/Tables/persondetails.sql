655e731ebdbceb5089f4200aa849b5f51fb91f9a
Rohit Jha <53367041+rohitjha20@users.noreply.github.com>
2026-07-22 16:53:22 +0530

    Create person table with relevant fields

---
+++ dbo/Tables/persondetails.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[person] (
+    [PersonID]   INT             IDENTITY (1, 1) NOT NULL,
+    [Personname] NVARCHAR (100)  NOT NULL,
+    [Relation]   NVARCHAR (50)   NULL,
+    [Salary]       DECIMAL (10, 2) NULL,
+    [JoiningDate]  DATE            NULL,
+    [EmailID]       NVARCHAR (200) NULL,
+    [PhoneNumber]   NVARCHAR (15)  NULL,
+    [Address]   NVARCHAR (100)  NULL,
+    PRIMARY KEY CLUSTERED ([EmployeeID] ASC)
+);
+GO
